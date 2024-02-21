5b38ec852d4f7ea4c0b96127cfaa2d7f72bfb3d9
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-02-21 22:46:58 +0600

    feat: insert data in table books

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO books (title, author, genre)
+VALUES ('Alice in the Wonderland', 'Lewis Carol', 'tales'),
+       ('Hobbit', 'John R.R. Tolkien', 'fantasy'),
+       ('The Leader In You', 'Dale Carnegie', 'psychology');
