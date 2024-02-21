e40bdb259e71bf8d74c84d5861111fe00ddaa4d7
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-02-21 22:50:25 +0600

    feat: insert data in table readers

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,7 @@ INSERT INTO books (title, author, genre)
 VALUES ('Alice in the Wonderland', 'Lewis Carol', 'tales'),
        ('Hobbit', 'John R.R. Tolkien', 'fantasy'),
        ('The Leader In You', 'Dale Carnegie', 'psychology');
+INSERT INTO readers (id, name)
+VALUES (1, 'John Smith'),
+       (2, 'Robert Walker'),
+       (3, 'David Jameson');
