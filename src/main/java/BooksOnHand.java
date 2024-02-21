c94f7fef2f2c9391ae993dab9d079d8df19c27e6
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-02-21 23:28:26 +0600

    feat: create class BooksOnHand, add annotations

---
+++ src/main/java/BooksOnHand.java
@@ -0,0 +1,24 @@
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "books_on_hand")
+public class BooksOnHand {
+    @Id
+    private int id;
+    private int bookId;
+    private int readerId;
+    private LocalDateTime issuedDate;
+    private LocalDateTime returnDate;
+}
