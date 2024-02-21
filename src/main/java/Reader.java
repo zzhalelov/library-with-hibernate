88d42ba0e04df6ba5f7bb65c726021d09af2096f
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-02-21 22:57:18 +0600

    feat: create class Reader, add annotations

---
+++ src/main/java/Reader.java
@@ -0,0 +1,19 @@
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "readers")
+public class Reader {
+    @Id
+    private int id;
+    private String name;
+}
