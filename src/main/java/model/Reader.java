6076365d0f5b0734514c2876a6c67f6ed2fdca27
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-02-27 23:33:20 +0600

    feat: private Set<BookStatus> booksOnHand

--- src/main/java/model/Reader.java
+++ src/main/java/model/Reader.java
@@ -3,6 +3,8 @@ package model;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -17,4 +19,7 @@ public class Reader {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+
+    @OneToMany(mappedBy = "reader")
+    private Set<BookStatus> booksOnHand = new HashSet<>();
 }
