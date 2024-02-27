86ade2543684f6c5a369a4bc260a23a2de56bbb6
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-02-27 23:04:17 +0600

    fix: add @GeneratedValue(strategy = GenerationType.IDENTITY)

--- src/main/java/model/Reader.java
+++ src/main/java/model/Reader.java
@@ -2,9 +2,7 @@ package model;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -16,6 +14,7 @@ import javax.persistence.Table;
 @Table(name = "readers")
 public class Reader {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
 }
