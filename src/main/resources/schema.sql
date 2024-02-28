8eaffc033fd9702649d59068ca92a5eb6f2a3bf0
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-02-28 23:40:24 +0600

    fix: rename columns

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,16 +5,20 @@ CREATE TABLE books
     author VARCHAR,
     genre  VARCHAR
 );
+ALTER TABLE books
+    RENAME id To id_book;
 CREATE TABLE readers
 (
     id   INT PRIMARY KEY,
     name VARCHAR
 );
+ALTER TABLE readers
+    RENAME id TO id_reader;
 CREATE TABLE books_on_hand
 (
     id          SERIAL PRIMARY KEY,
-    book_id     INT REFERENCES books (id),
-    reader_id   INT REFERENCES readers (id),
+    book_id     INT REFERENCES books (id_book),
+    reader_id   INT REFERENCES readers (id_reader),
     issued_date DATE,
     return_date DATE
 );
@@ -26,6 +30,6 @@ CREATE TABLE book_statuses
 CREATE TABLE books_in_library
 (
     id        SERIAL PRIMARY KEY,
-    book_id   INT REFERENCES books (id),
+    book_id   INT REFERENCES books (id_book),
     status_id INT REFERENCES book_statuses (id)
 );
