77f7639e89847f786403163edb2e747a837eda36
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-02-28 22:23:03 +0600

    feat: CREATE TABLE book_in_library

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -23,3 +23,9 @@ CREATE TABLE book_statuses
     id     SERIAL PRIMARY KEY,
     status VARCHAR
 );
+CREATE TABLE book_in_library
+(
+    id        SERIAL PRIMARY KEY,
+    book_id   INT REFERENCES books (id),
+    status_id INT REFERENCES book_statuses (id)
+);
